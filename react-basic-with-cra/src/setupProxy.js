5a3e974fa958f52f61a42dd82ea4be6907769f07
wangyaochong <1162025261@qq.com>
2020-04-01 21:02:17 +0800

    saga学习，主要用于异步回调

--- react-basic-with-cra/src/setupProxy.js
+++ react-basic-with-cra/src/setupProxy.js
@@ -1,9 +1,28 @@
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
+    // app.use(
+    //     '/api', createProxyMiddleware({
+    //         target: 'http://127.0.0.1:3100/api',
+    //         changeOrigin: true
+    //     })
+    // );
+    // app.use(
+    //     'http://127.0.0.1:3000/login', createProxyMiddleware({
+    //         target: 'http://127.0.0.1:3100/xxx',
+    //         changeOrigin: true
+    //     })
+    // );
     app.use(
-        '/api', proxy({
-            target: 'http://localhost:3100',
-            changeOrigin: true
+        '/login', createProxyMiddleware({
+            target: 'http://127.0.0.1:7001',
+            changeOrigin: true,
         })
     );
+    // app.use(
+    //     '/', createProxyMiddleware({
+    //         target: 'http://localhost:3100/login',
+    //         changeOrigin: true
+    //     })
+    // );
 };
